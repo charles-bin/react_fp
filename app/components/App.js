05825ea252582b2e50a0548d988d13c1c0eb832d
Charles Bin <charles.bin@gmail.com>
2017-07-09 17:24:33 +0700

    Input tick

--- app/components/App.js
+++ app/components/App.js
@@ -13,34 +13,91 @@ const styles = {
 export class App extends React.Component {
   constructor(props) {
     super(props);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+
+    this.inputTick = this.inputTick.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+
     this.state = {
-      top: 10,
-      left: 10,
+      top: 0,
+      left: 0,
+      keydownLeft: false,
+      keydownUp: false,
+      keydownRight: false,
+      keydownDown: false,
     };
   }
 
   componentWillMount() {
-    document.addEventListener("keydown", this.handleKeyPress);
+    document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener("keyup", this.handleKeyUp);
   }
 
-  handleKeyPress(event) {
-    switch (event.keyCode) {
-      case 37:
-        this.setState({ left: this.state.left -= 20 });
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.inputTick(),
+      10
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  inputTick() {
+    const v = 10;
+    const dx = (this.state.keydownLeft && -v) + (this.state.keydownRight && v);
+    const dy = (this.state.keydownUp && -v) + (this.state.keydownDown && v);
+
+    this.setState({
+      left: this.state.left + dx,
+      top: this.state.top + dy,
+    });
+    //console.log('state: ' + JSON.stringify(this.state));
+  }
+
+  handleKeyDown(event) {
+    switch (event.key) {
+      case 'a':
+        if(!this.state.keydownLeft) {
+          this.setState({ keydownLeft: true });
+        }
         break;
-      case 38:
-        this.setState({ top: this.state.top -= 20 });
+      case 'w':
+        if(!this.state.keydownUp) {
+          this.setState({ keydownUp: true });
+        }
         break;
-      case 39:
-        this.setState({ left: this.state.left += 20 });
+      case 'd':
+        if(!this.state.keydownRight) {
+          this.setState({ keydownRight: true });
+        }
         break;
-      case 40:
-        this.setState({ top: this.state.top += 20 });
+      case 's':
+        if(!this.state.keydownDown) {
+          this.setState({ keydownDown: true });
+        }
         break;
     }
-    //console.log("handleKeyPress: " + event.keyCode);
-    //console.log("top: " + this.state.top + " --- left: " + this.state.left);
+    //console.log("handleKeyDown: " + event.key);
+  }
+
+  handleKeyUp(event) {
+    switch (event.key) {
+      case 'a':
+        this.setState({ keydownLeft: false });
+        break;
+      case 'w':
+        this.setState({ keydownUp: false });
+        break;
+      case 'd':
+        this.setState({ keydownRight: false });
+        break;
+      case 's':
+        this.setState({ keydownDown: false });
+        break;
+    }
+    //console.log("handleKeyUp: " + event.key);
   }
 
   render() {
